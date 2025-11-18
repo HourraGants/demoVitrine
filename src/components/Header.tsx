bb4e600f941a5bce3c3b0aefe235d49914e3bb86
unknown <arthur.saussereau@hotmail.fr>
2025-11-18 19:33:42 +0100

    fixing the last elements, like applying the responsive of the header, hero section and the form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,37 @@
+import { useState } from 'react';
 import '../theme.css';
+import { Menu, X } from "lucide-react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggle = () => {
+    setIsOpen(prev => !prev);
+  };
+
   return (
-    <div id="header">
+    <header id="header">
       <div id="logo">
         <p>CARPENTER</p>
       </div>
-      <nav>
+      <button
+        className="header-burger"
+        aria-expanded={isOpen}
+        aria-label="Menu"
+        onClick={handleToggle}
+      >
+        {isOpen ? <X color="white" /> : <Menu color="white" />}
+      </button>
+      <nav className={`header-nav ${isOpen ? "nav-open" : ""}`}>
         <ul>
-          <li><a href="#home" className="header-nav">Home</a></li>
-          <li><a href="#services" className="header-nav">Services</a></li>
-          <li><a href="#process" className="header-nav">Process</a></li>
-          <li><a href="#works" className="header-nav">Works</a></li>
-          <li><a href="#contact" className="header-nav">Contact</a></li>
+          <li><a href="#home" className="header-nav-li">Home</a></li>
+          <li><a href="#services" className="header-nav-li">Services</a></li>
+          <li><a href="#process" className="header-nav-li">Process</a></li>
+          <li><a href="#works" className="header-nav-li">Works</a></li>
+          <li><a href="#contact" className="header-nav-li">Contact</a></li>
           <li><img src="./src/assets/search.png" title="Searchbar" alt="search" /></li>
         </ul>
       </nav>
-    </div>
+    </header>
   )
 }
 
